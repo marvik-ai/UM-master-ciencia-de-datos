a1d845f145d46baa6b1dc0aa499260831f44383e
santiagoferreiros <santiago@marvik.ai>
2025-04-23 17:58:43 -0300

    UM - Clase 1

---
+++ run_api.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Iniciar Streamlit (Si no funciona ejecutar una primera vez streamlit fuera del script)
+streamlit run front_streamlit.py --server.port=8501 --server.headless=True &
+
+# Iniciar API (FastAPI)
+uvicorn app:app --host 0.0.0.0 --port 8000
+
+# Esperar a que cualquier proceso en segundo plano termine (opcional)
+wait
